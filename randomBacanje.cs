554ac98fc5100ddebf5d61fc356f8cd6ef87613c
Edin387SA <126198664+Edin387SA@users.noreply.github.com>
2023-08-05 23:34:44 +0200

    Add files via upload

---
+++ randomBacanje.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+
+namespace CovjeceNeLjutiSe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          Random random = new Random();
+           int bacanje = random.Next(1, 7);
+            int bacanje1 = random.Next(1, 7);
+            int bacanje2 = random.Next(1, 7);
+            Console.WriteLine(bacanje);
+            Console.WriteLine(bacanje1);
+            Console.WriteLine(bacanje2);
+
+            Console.ReadKey();
+            Console.Beep();
+        }
+    }
+}
